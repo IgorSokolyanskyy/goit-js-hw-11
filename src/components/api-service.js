19ab8295c067381c2b1b316306552072526314ec
IgorSokolyanskyy <1988isokol@gmail.com>
2022-12-04 00:31:41 +0200

    исправляет ошибки

---
+++ src/components/api-service.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/?';
+const API_KEY = '31669941-43ed95656cb1213a9578e127d';
+
+const searchParams = new URLSearchParams({
+  key: API_KEY,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+  per_page: 40,
+});
+
+class ImagesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchImages() {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}${searchParams}&q=${this.searchQuery}&page=${this.page}`
+      );
+      this.incrementPage();
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
+
+export { ImagesApiService };
--- src/index.js
+++ src/index.js
@@ -1,56 +1,140 @@
-import './css/styles.css';
-import { fetchCountries } from './js/fetchCountries';
-import { countryСardTeemplate, countryListTemplate } from './js/markupTemplate';
-import { refs } from './js/refs-elements';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import debounce from 'lodash.debounce';
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { InfiniteScroll } from 'infinite-scroll';
+import './css/styles.css';
+import { ImagesApiService } from './components/api-service';
+// import { createImagesMarkup } from './components/createImagesMarkup';
 
-const DEBOUNCE_DELAY = 300;
-refs.searchBox.addEventListener(
-  'input',
-  debounce(onInputCountry, DEBOUNCE_DELAY)
-);
+const refs = {
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
 
-function onInputCountry(e) {
+refs.form.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+const imagesApiService = new ImagesApiService();
+
+let gallery = new SimpleLightbox('.photo-card a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+let imagesShown = 0;
+
+function onFormSubmit(e) {
   e.preventDefault();
+  clearMarkUp();
+  refs.loadMoreBtn.classList.add('is-hidden');
 
-  const countryName = refs.searchBox.value.trim();
+  imagesShown = 0;
 
-  if (countryName === '') {
-    refs.countryInfo.innerHTML = '';
-    refs.countryList.innerHTML = '';
+  imagesApiService.resetPage();
+
+  imagesApiService.query = e.currentTarget.elements.searchQuery.value.trim();
+
+  if (!imagesApiService.query) {
+    Notify.info('Empty request, please type not only spaces');
     return;
   }
 
-  fetchCountries(countryName)
-    .then(countrys => {
-      if (countrys.length > 10) {
-        Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-        refs.countryInfo.innerHTML = '';
-        refs.countryList.innerHTML = '';
-        return;
-      }
+  fetchImages();
+}
 
-      if (countrys.length <= 10) {
-        const listMarkup = countrys.map(country =>
-          countryListTemplate(country)
-        );
-        refs.countryList.innerHTML = listMarkup.join('');
-        refs.countryInfo.innerHTML = '';
-      }
+async function fetchImages() {
+  try {
+    const { hits, totalHits, total } = await imagesApiService.fetchImages();
 
-      if (countrys.length === 1) {
-        const markup = countrys.map(country => countryСardTeemplate(country));
-        refs.countryInfo.innerHTML = markup.join('');
-        refs.countryList.innerHTML = '';
+    if (!hits.length) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    createImagesMarkup(hits);
+
+    if (imagesApiService.page > 2) {
+      smoothScroll();
+    }
+
+    if (!imagesShown) {
+      Notify.success(
+        `We found ${totalHits} , buy licence to get more, total found ${total}`
+      );
+    }
+    imagesShown += hits.length;
+    gallery.refresh();
+
+    if (imagesShown < totalHits) {
+      refs.loadMoreBtn.classList.remove('is-hidden');
+    } else {
+      refs.loadMoreBtn.classList.add('is-hidden');
+      Notify.info(
+        `We are sorry, but you have reached the end of search results. Totally shown: ${imagesShown} images`
+      );
+    }
+  } catch (error) {
+    Notify.failure(`${error}`);
+  }
+}
+
+function createImagesMarkup(imagesData) {
+  const markup = imagesData
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card"><a href="${largeImageURL}">
+  <img src="${webformatURL}" width='350' height='250' alt="${tags}" loading="lazy"/></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes <span>${likes}</span> </b>
+    </p>
+    <p class="info-item">
+      <b>Views <span> ${views}</span></b>
+    </p>
+    <p class="info-item">
+      <b>Comments <span>${comments}</span></b>
+    </p>
+    <p class="info-item">
+      <b>Downloads <span>${downloads}</span></b>
+
+    </p>
+  </div>
+</div>`;
       }
-    })
-    .catch(error => {
-      Notify.failure('Oops, there is no country with that name');
-      refs.countryInfo.innerHTML = '';
-      refs.countryList.innerHTML = '';
-      return error;
-    });
+    )
+    .join(' ');
+
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+function clearMarkUp() {
+  refs.gallery.innerHTML = ``;
+}
+
+function onLoadMore() {
+  fetchImages();
 }
